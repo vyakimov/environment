7aa532f2f425dd1d405e96e76d69a5d8486ad966
Victor <vya@isynet.net>
2025-07-24 08:47:49 +0000

    multi-stage build, consolidated environments

--- build.sh
+++ build.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 case "$1" in
 build)
-  docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg PYTHON_V=3.10.13 -f Dockerfile.environment"$2" -t environment"$2":latest .
+  echo "building environment"
+  docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg PYTHON_V=3.10.13 -f Dockerfile.environment"$2" -t environment:latest .
   ;;
 rebuild)
-  docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg PYTHON_V=3.10.13 -f Dockerfile.environment"$2" -t environment"$2":latest --no-cache .
+  echo "force building environment"
+  docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg PYTHON_V=3.10.13 -f Dockerfile.environment"$2" -t environment:latest --no-cache .
+  ;;
+python_base)
+  echo "building python base"
+  docker build \
+    --build-arg USER_ID=$(id -u) \
+    --build-arg GROUP_ID=$(id -g) \
+    --build-arg PYTHON_V=3.10.13 \
+    -f Dockerfile.pythonBase \
+    -t python_base:latest .
+  ;;
+run)
+  echo "running environment"
+  docker run \
+    -it \
+    --rm \
+    -v $PWD:/app/ \
+    --env-file .secrets \
+    environment /bin/zsh
   ;;
 *)
-  echo "Usage: $0 {build|rebuild}"
+  echo "Usage: $0 {build|rebuild|run}"
   exit 1
   ;;
 esac
