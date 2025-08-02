8ee262beff60701352f233e06100e690f7b33954
Victor <vya@isynet.net>
2025-08-02 14:54:05 +0000

    pyenv switched out for uv

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
-docker run \
-  -it \
-  --rm \
-  -v $PWD:/app/ \
-  -v $HOME/.ssh/:/home/user/.ssh/ \
-  --env-file .secrets \
-  environment /bin/zsh
+#!/bin/bash
+
+if [[ "$1" == "root" ]]; then
+  docker run \
+    -it \
+    --rm \
+    --net=host \
+    -v "$PWD":/app/ \
+    -v "$HOME"/.ssh/:/home/user/.ssh/ \
+    --env-file .secrets \
+    --user root \
+    environment /bin/zsh
+else
+  docker run \
+    -it \
+    --rm \
+    --net=host \
+    -v "$PWD":/app/ \
+    -v "$HOME"/.ssh/:/home/user/.ssh/ \
+    --env-file .secrets \
+    environment /bin/zsh
+fi
